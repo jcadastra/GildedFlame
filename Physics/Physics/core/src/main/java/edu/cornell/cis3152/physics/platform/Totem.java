5bb35a04ae689cfd37cd3321a0352f146475667c
harveyzhu27 <158126112+harveyzhu27@users.noreply.github.com>
2025-03-02 13:07:28 -0500

    Added update function to AIController

--- Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/platform/Totem.java
+++ Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/platform/Totem.java
@@ -1,15 +1,16 @@
 package edu.cornell.cis3152.physics.platform;
 
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 
 public class Totem extends Enemy {
-    public Totem(int id, Vector2 pos) {
-        super(id, pos);
+    public Totem(int id, Vector2 pos, Body body) {
+        super(id, pos, body);
     }
 
     // reacting to light
     @Override
-    public void react(){
+    public void in_light(){
         // turns solid
     }
 
