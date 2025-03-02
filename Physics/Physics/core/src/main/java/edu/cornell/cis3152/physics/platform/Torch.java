082aa8071e04a1d87fc7b20364ebdf6c1988bffb
EricJudys1 <egj29@cornell.edu>
2025-03-02 15:58:48 -0500

    Added in torch and light class, modified constants/put torch in own level

---
+++ Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/platform/Torch.java
@@ -0,0 +1,195 @@
+package edu.cornell.cis3152.physics.platform;
+
+import com.badlogic.gdx.math.*;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.physics.box2d.*;
+
+import com.badlogic.gdx.utils.JsonValue;
+import edu.cornell.gdiac.assets.ParserUtils;
+import edu.cornell.gdiac.graphics.SpriteBatch;
+import edu.cornell.gdiac.graphics.Texture2D;
+import edu.cornell.gdiac.math.Path2;
+import edu.cornell.gdiac.math.PathFactory;
+import edu.cornell.gdiac.physics2.*;
+
+public class Torch extends ObstacleSprite {
+
+    private final JsonValue data;
+    private final float width;
+    private final float height;
+    private Path2 sensorOutline;
+    private Color sensorColor;
+    private String sensorName;
+
+    /**Whether the torch is in player's hand*/
+    private boolean isOnHand;
+
+    /**Query whether torch is in player's hand or not*/
+    public boolean getOnHand() {return isOnHand;}
+
+    /**Set whether torch is in player's hand or not*/
+    public void setOnHand(boolean v) {isOnHand = v;}
+
+    public Torch(float units, JsonValue data) {
+        this.data = data;
+        JsonValue debugInfo = data.get("debug");
+
+        float x = data.get("pos").getFloat(0);
+        float y = data.get("pos").getFloat(1);
+        float s = data.getFloat( "size" );
+        float size = s*units;
+
+        // The capsule is smaller than the image
+        // "inner" is the fraction of the original size for the capsule
+        width = s*data.get("dimensions").getFloat(0);
+        height = s*data.get("dimensions").getFloat(1);
+        obstacle = new CapsuleObstacle(x, y, width, height);
+        ((CapsuleObstacle)obstacle).setTolerance( debugInfo.getFloat("tolerance", 0.5f) );
+
+        obstacle.setDensity( data.getFloat( "density", 0 ) );
+        obstacle.setFriction( data.getFloat( "friction", 0 ) );
+        obstacle.setRestitution( data.getFloat( "restitution", 0 ) );
+        obstacle.setFixedRotation(true);
+        obstacle.setPhysicsUnits( units );
+        obstacle.setUserData( this );
+        obstacle.setName("torch");
+
+        debug = ParserUtils.parseColor( debugInfo.get("avatar"),  Color.WHITE);
+        sensorColor = ParserUtils.parseColor( debugInfo.get("sensor"),  Color.WHITE);
+
+        // Gameplay attributes
+        isOnHand = false;
+
+        // Create a rectangular mesh for Traci. This is the same as for door,
+        // since Traci is a rectangular image. But note that the capsule is
+        // actually smaller than the image, making a tighter hitbox. You can
+        // see this when you enable debug mode.
+        mesh.set(-size/2.0f,-size/2.0f,size,size);
+    }
+
+    /**
+     * Creates the sensor for Traci.
+     *
+     * We only allow the Traci to jump when she's on the ground. Double jumping
+     * is not allowed.
+     *
+     * To determine whether Traci is on the ground we create a thin sensor under
+     * her feet, which reports collisions with the world but has no collision
+     * response. This sensor is just a FIXTURE, it is not an obstacle. We will
+     * talk about the different between these later.
+     *
+     * Note this method is not part of the constructor. It can only be called
+     * once the physics obstacle has been activated.
+     */
+    public void createSensor() {
+        Vector2 sensorCenter = new Vector2(0, -height / 2);
+        FixtureDef sensorDef = new FixtureDef();
+        sensorDef.density = data.getFloat("density",0);
+        sensorDef.isSensor = true;
+
+        JsonValue sensorjv = data.get("sensor");
+        float w = sensorjv.getFloat("shrink",0)*width/2.0f;
+        float h = sensorjv.getFloat("height",0);
+        PolygonShape sensorShape = new PolygonShape();
+        sensorShape.setAsBox(w, h, sensorCenter, 0.0f);
+        sensorDef.shape = sensorShape;
+
+        // Ground sensor to represent our feet
+        Body body = obstacle.getBody();
+        Fixture sensorFixture = body.createFixture( sensorDef );
+        sensorName = "traci_sensor";
+        sensorFixture.setUserData(sensorName);
+
+        // Finally, we need a debug outline
+        float u = obstacle.getPhysicsUnits();
+        PathFactory factory = new PathFactory();
+        sensorOutline = new Path2();
+        factory.makeRect( (sensorCenter.x-w/2)*u,(sensorCenter.y-h/2)*u, w*u, h*u,  sensorOutline);
+    }
+
+
+    /**
+     * Applies the force to the body of Traci
+     *
+     * This method should be called after the force attribute is set.
+     */
+    public void applyThrowForce() {
+        if (!obstacle.isActive()) {
+            return;
+        }
+
+//        Vector2 pos = obstacle.getPosition();
+//        float vx = obstacle.getVX();
+//        Body body = obstacle.getBody();
+//
+//        // Don't want to be moving. Damp out player motion
+//        if (getMovement() == 0f) {
+//            forceCache.set(-getDamping()*vx,0);
+//            body.applyForce(forceCache,pos,true);
+//        }
+//
+//        // Velocity too high, clamp it
+//        if (Math.abs(vx) >= getMaxSpeed()) {
+//            obstacle.setVX(Math.signum(vx)*getMaxSpeed());
+//        } else {
+//            forceCache.set(getMovement(),0);
+//            body.applyForce(forceCache,pos,true);
+//        }
+//
+//        // Jump!
+//        if (isJumping()) {
+//            forceCache.set(0, jump_force);
+//            body.applyLinearImpulse(forceCache,pos,true);
+//        }
+    }
+
+    /**
+     * Updates the object's physics state (NOT GAME LOGIC).
+     *
+     * We use this method to reset cooldowns.
+     *
+     * @param dt    Number of seconds since last animation frame
+     */
+
+    /**
+     * Draws the physics object.
+     *
+     * This method is overridden from ObstacleSprite. We need to flip the
+     * texture back-and-forth depending on her facing. We do that by creating
+     * a reflection affine transform.
+     *
+     * @param batch The sprite batch to draw to
+     */
+
+    /**
+     * Draws the outline of the physics object.
+     *
+     * This method is overridden from ObstacleSprite. By default, that method
+     * only draws the outline of the main physics obstacle. We also want to
+     * draw the outline of the sensor, and in a different color. Since it
+     * is not an obstacle, we have to draw that by hand.
+     *
+     * @param batch The sprite batch to draw to
+     */
+    @Override
+    public void drawDebug(SpriteBatch batch) {
+        super.drawDebug( batch );
+
+        if (sensorOutline != null) {
+            batch.setTexture( Texture2D.getBlank() );
+            batch.setColor( sensorColor );
+
+            Vector2 p = obstacle.getPosition();
+            float a = obstacle.getAngle();
+            float u = obstacle.getPhysicsUnits();
+
+            // transform is an inherited cache variable
+            transform.idt();
+            transform.preRotate( (float) (a * 180.0f / Math.PI) );
+            transform.preTranslate( p.x * u, p.y * u );
+
+            //
+            batch.outline( sensorOutline, transform );
+        }
+    }
+}
