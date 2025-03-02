645ae0ead5731bc781427e556b95864b9214b464
harveyzhu27 <158126112+harveyzhu27@users.noreply.github.com>
2025-03-02 12:50:32 -0500

    Added Totem and Moth classes and updated AIController

--- Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/platform/AIController.java
+++ Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/platform/AIController.java
@@ -1,9 +1,10 @@
 package edu.cornell.cis3152.physics.platform;
-
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+import java.util.List;
 public class AIController {
-    // public class AIController implements InputController
 
-    // Constants for chase algorithms
+    // Constants
     /**
      * How close the light must be for the moth to chase it
      */
@@ -17,4 +18,27 @@ public class AIController {
      * The number of ticks since we started this controller
      */
     private long ticks;
+
+    private static final float LIGHT_RADIUS = 3;
+
+    private List<Enemy> enemies;
+    // CHANGE LATER
+    public static Vector2 lightPosition;
+
+    public AIController(List<Enemy> enemies, Vector2 lightPos){
+        this.enemies = enemies;
+        this.lightPosition = lightPos;
+    }
+    public void update(){
+        for (Enemy enemy : enemies){
+            if (isInLight(enemy)){
+
+            }
+        }
+    }
+
+    public boolean isInLight(Enemy enemy){
+        float distance = enemy.getPosition().dst(lightPosition);
+        return distance < LIGHT_RADIUS;
+    }
 }
