b0958facab83e83eca08fe4ed3dcbb03a815efe3
EricJudys1 <egj29@cornell.edu>
2025-03-06 13:50:20 -0500

    reimplementing + missing changes in dev

---
+++ Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/level_player/CollisionController.java
@@ -0,0 +1,30 @@
+package edu.cornell.cis3152.physics.level_player;
+
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Manifold;
+import edu.cornell.cis3152.physics.level_player.Torch_playground;
+
+public class CollisionController implements ContactListener {
+
+    @Override
+    public void beginContact(Contact contact) {
+
+    }
+
+    @Override
+    public void endContact(Contact contact) {
+
+    }
+
+    @Override
+    public void preSolve(Contact contact, Manifold manifold) {
+
+    }
+
+    @Override
+    public void postSolve(Contact contact, ContactImpulse contactImpulse) {
+
+    }
+}
