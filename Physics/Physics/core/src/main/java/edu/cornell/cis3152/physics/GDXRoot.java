9b34b6ad64124b4165f548bc5bee0617d4747dac
Cynthia-Sun-929 <ys728@cornell.edu>
2025-03-10 23:55:27 -0400

    create a working level loader with collision handling errors

--- Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/GDXRoot.java
+++ Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/GDXRoot.java
@@ -45,6 +45,10 @@ public class GDXRoot extends Game implements ScreenListener {
     /** List of all WorldControllers */
     private GameplayScene[] controllers;
 
+    private String[] levels;
+
+    private GameplayScene currentScene;
+
     /**
      * Creates a new game from the configuration settings.
      *
@@ -135,7 +139,14 @@ public class GDXRoot extends Game implements ScreenListener {
             loading = null;
 
             // Initialize the three game worlds
-            controllers = new GameplayScene[1];
+            currentScene = new GameplayScene(directory, "platform");
+            levels = new String[1];
+            levels[0] = "moth_intro";
+            currentScene.loadLevel(levels[0]);
+            currentScene.setScreenListener(this);
+            currentScene.setSpriteBatch(batch);
+
+            // controllers = new GameplayScene[1];
 //            controllers[0] = new RocketScene(directory);
             //controllers[0] = new og_platformer(directory);
 //            controllers[1] = new empty_layout1(directory);
@@ -143,26 +154,29 @@ public class GDXRoot extends Game implements ScreenListener {
 //            controllers[0] = new Totem_intro(directory);
 //            controllers[1] = new Moth_intro(directory);
 //            controllers[2] = new Torch_playground(directory);
-            controllers[0] = new GameplayScene_temp(directory, "temp");
+//            controllers[2] = new GameplayScene_temp(directory, "temp");
 //            controllers[2] = new RagdollScene(directory);
 //            controllers[3] = new PlatformScene(directory);
 
-            for(int ii = 0; ii < controllers.length; ii++) {
+            /*for(int ii = 0; ii < controllers.length; ii++) {
                 controllers[ii].setScreenListener(this);
                 controllers[ii].setSpriteBatch(batch);
-            }
+            }*/
 
             current = 0;
-            controllers[current].reset();
-            setScreen(controllers[current]);
+            currentScene.reset();
+            setScreen(currentScene);
+            // controllers[current].reset();
+            // setScreen(controllers[current]);
         } else if (exitCode == GameplayScene.EXIT_NEXT) {
-            current = (current+1) % controllers.length;
+            current = (current+1) % levels.length;
+            /*current = (current+1) % controllers.length;
             controllers[current].reset();
-            setScreen(controllers[current]);
+            setScreen(controllers[current]);*/
         } else if (exitCode == GameplayScene.EXIT_PREV) {
-            current = (current+controllers.length-1) % controllers.length;
-            controllers[current].reset();
-            setScreen(controllers[current]);
+            current = (current+levels.length-1) % levels.length;
+            /*controllers[current].reset();
+            setScreen(controllers[current]);*/
         } else if (exitCode == GameplayScene.EXIT_QUIT) {
             // We quit the main application
             Gdx.app.exit();
