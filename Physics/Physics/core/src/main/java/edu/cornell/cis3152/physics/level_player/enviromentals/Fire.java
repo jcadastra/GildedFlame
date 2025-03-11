3383b16df9f6be419615061ac07c0fb9d23de111
EricJudys1 <egj29@cornell.edu>
2025-03-10 23:43:51 -0400

    misc

--- Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/level_player/enviromentals/Fire.java
+++ Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/level_player/enviromentals/Fire.java
@@ -21,9 +21,6 @@ public class Fire extends ObstacleSprite {
     }
 
     public Boolean queryPointInside(Vector2 point) {
-//        System.out.println("fire pos" + obstacle.getPosition());
-//        System.out.println("alt point" + point);
-//        System.out.println("ALERT ON " + point);
         return (obstacle.getPosition()).sub(point).len() <= radius;
     }
 
@@ -69,24 +66,3 @@ public class Fire extends ObstacleSprite {
         return (obstacle.getPosition().toString());
     }
 }
-
-/**
- * TODO:
- *
- * Given a fire, needs to burn
- * so a singular fire needs to have a light source and partcile generator
- *
- * one or spreading?
- * one i think
- * given a polygon, randomly place points within it by triangulation
- *
- * need fire controller for spreading
- *      can have dictionary from body to n distribution
- * fire to generate things
- *
- * for each block on fire not all lit
- *      get point closet to next fire on blcok, light it with material based lighting timer
- *          add contact hangler that if fire git burnable block but is not on fire then add to fire
- *      if all points then set off internal material lighitng timer
- * upon expiration of internal block lighting timer, destroy
- */
