996c334e74221d6ba8b8f4f2be4da0aeb223329c
harveyzhu27 <158126112+harveyzhu27@users.noreply.github.com>
2025-03-16 15:12:48 -0400

    Random commit

--- Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/GDXRoot.java
+++ Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/GDXRoot.java
@@ -138,10 +138,10 @@ public class GDXRoot extends Game implements ScreenListener {
 //            controllers[1] = new empty_layout1(directory);
 //            controllers[2] = new empty_layout2(directory);
 //            controllers[0] = new Totem_intro(directory);
-            controllers[0] = new Moth_intro(directory);
+//            controllers[0] = new Moth_intro(directory);
 //            controllers[1] = new Torch_playground(directory);
 //            controllers[1] = new GameplayScene_temp(directory, "temp");
-//            controllers[0] = new Totem_playground(directory);
+            controllers[0] = new Totem_playground(directory);
 //            controllers[2] = new RagdollScene(directory);
 //            controllers[3] = new PlatformScene(directory);
 
