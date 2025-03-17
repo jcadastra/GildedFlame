12bfed86fc61f9f84341c71cee18c0aee466f0c1
Judy <yz2429@cornell.edu>
2025-03-16 23:42:05 -0400

    minor level tweaks

--- Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/GDXRoot.java
+++ Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/GDXRoot.java
@@ -163,8 +163,8 @@ public class GDXRoot extends Game implements ScreenListener {
             levels = new String[4];
             levels[0] = "moth_intro";
             levels[1] = "totem_intro";
-            levels[2] = "spinner_layout";
-            levels[3] = "rope_test";
+            //levels[2] = "spinner_layout";
+            levels[2] = "rope_test";
             currentScene.loadLevel(levels[0]);
             currentScene.setScreenListener(this);
             currentScene.setSpriteBatch(batch);
