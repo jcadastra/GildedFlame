35e57998762264ad4c0c3e470c774c1e9feeb107
Cynthia-Sun-929 <ys728@cornell.edu>
2025-04-07 17:41:11 -0400

    merge

---
+++ Physics/Physics/assets/tileset.tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tileset" tilewidth="300" tileheight="300" tilecount="8" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../../../Downloads/edgeVert1_040125.png" width="300" height="300"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="33" y="0" width="105" height="300"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <image source="../../../../Downloads/edgeTop1_040125.png" width="300" height="300"/>
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="6" width="300" height="150"/>
+   <object id="3" x="0" y="6" width="300" height="150"/>
+   <object id="5" x="0" y="6" width="300" height="150"/>
+   <object id="6" x="0" y="6" width="300" height="150"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <image source="../../../../Downloads/edgeEnd1_040125.png" width="300" height="300"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="146.355" y="7" width="153.645" height="129.388"/>
+   <object id="2" x="143.645" y="6.32234" width="155" height="126"/>
+   <object id="3" x="145" y="7" width="155" height="126"/>
+   <object id="4" x="145" y="7" width="155" height="126"/>
+   <object id="5" x="145" y="7" width="155" height="126"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <image source="../../../../Downloads/edgeCorner3_040125.png" width="300" height="300"/>
+ </tile>
+ <tile id="4">
+  <image source="../../../../Downloads/edgeCorner2_040125.png" width="300" height="300"/>
+ </tile>
+ <tile id="5">
+  <image source="../../../../Downloads/edgeCorner1_040125.png" width="300" height="300"/>
+ </tile>
+ <tile id="6">
+  <image source="../../../../Downloads/edgeBottom1_040125.png" width="300" height="300"/>
+ </tile>
+ <tile id="7">
+  <image source="../../../../Downloads/Stone_tile_set_03_27_25.png" width="300" height="300"/>
+ </tile>
+</tileset>
