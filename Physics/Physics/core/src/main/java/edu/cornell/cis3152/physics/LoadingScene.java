ed86a8cb7958ec950621721ec1021eacde99aa53
Judy <yz2429@cornell.edu>
2025-04-15 17:06:57 -0400

    minor

--- Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/LoadingScene.java
+++ Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/LoadingScene.java
@@ -323,14 +323,14 @@ public class LoadingScene implements Screen, InputProcessor {
 
             if (inputController.isUsingController()) {//controller support
                 boolean pressA = inputController.xbox.getA();
-                if (pressA && !prevButtonA) {
+                if (pressA && !prevButtonA) {//simulate push down
                     pressState = 1;
-                    System.out.println("pressed");
+                    //System.out.println("pressed");
                 }
-                if (!pressA && prevButtonA && pressState == 1) {
+                if (!pressA && prevButtonA && pressState == 1) {//simulate push up
                     // A was just released
                     pressState = 2;
-                    System.out.println("released");
+                    //System.out.println("released");
                 }
                 prevButtonA = pressA;
             }
