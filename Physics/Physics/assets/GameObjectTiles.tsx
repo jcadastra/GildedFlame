4bfcfefd93cd6aedb04295b89b89dca7dbbfad9f
Cynthia-Sun-929 <ys728@cornell.edu>
2025-04-20 20:36:01 -0400

    add texture for rune in level editor

--- Physics/Physics/assets/GameObjectTiles.tsx
+++ Physics/Physics/assets/GameObjectTiles.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="GameObjectTiles" tilewidth="500" tileheight="500" tilecount="3" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="GameObjectTiles" tilewidth="500" tileheight="500" tilecount="7" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="2">
   <image source="tiled_images/ropeEnd.png" width="300" height="300"/>
@@ -9,5 +9,17 @@
  </tile>
  <tile id="4">
   <image source="shared/goaldoor.png" width="500" height="500"/>
+ </tile>
+ <tile id="8">
+  <image source="tiled_images/brick.png" width="300" height="300"/>
+ </tile>
+ <tile id="9">
+  <image source="tiled_images/column_background.png" width="300" height="500"/>
+ </tile>
+ <tile id="10">
+  <image source="tiled_images/rune.png" width="73" height="93"/>
+ </tile>
+ <tile id="11">
+  <image source="tiled_images/Window.png" width="300" height="500"/>
  </tile>
 </tileset>
