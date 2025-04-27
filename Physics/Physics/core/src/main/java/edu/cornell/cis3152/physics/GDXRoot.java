dcee88ebb135d6af9ab8a2058516a65af7c9ecf7
harveyzhu27 <158126112+harveyzhu27@users.noreply.github.com>
2025-04-27 00:12:26 -0400

    random commit

--- Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/GDXRoot.java
+++ Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/GDXRoot.java
@@ -190,7 +190,7 @@ public class GDXRoot extends Game implements ScreenListener {
 
                 // Initialize the gameplay scene and level data.
                 currentScene = new GameplayScene(directory, soundEngine, "platform");
-                levels = new String[]{"level1", "level7", "level8"};
+                levels = new String[]{"level1", "level7", "level9"};
                 currentScene.loadLevel(levels[selectedLevel - 1], "rope_test");
                 currentScene.setScreenListener(this);
                 currentScene.setSpriteBatch(batch);
