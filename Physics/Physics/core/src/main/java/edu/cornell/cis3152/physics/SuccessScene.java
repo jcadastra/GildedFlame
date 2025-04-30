24b652eaa42dc76bd5a299e8705c2d0d4afb010b
Cynthia-Sun-929 <ys728@cornell.edu>
2025-04-30 17:12:48 -0400

    add basic structure for success scene

---
+++ Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/SuccessScene.java
@@ -0,0 +1,65 @@
+package edu.cornell.cis3152.physics;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.ScreenUtils;
+import edu.cornell.gdiac.util.ScreenListener;
+import com.badlogic.gdx.audio.Sound;
+
+public class SuccessScene implements Screen {
+    private Stage stage;
+    private Skin skin;
+    private Texture bgTexture;
+    private boolean visible = false;;
+    private ScreenListener listener;
+    private Sound clickSound;
+    private InputController inputController = InputController.getInstance();
+    private boolean prevButtonA = false;
+
+    public SuccessScene() {
+        stage = new Stage(new ScreenViewport());
+        skin = new Skin();
+
+    }
+
+    @Override
+    public void show() {
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void hide() {
+        Gdx.input.setInputProcessor(null);
+    }
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {}
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+        skin.dispose();
+    }
+
+    public void setScreenListener(ScreenListener listener) {
+        this.listener = listener;
+    }
+}
