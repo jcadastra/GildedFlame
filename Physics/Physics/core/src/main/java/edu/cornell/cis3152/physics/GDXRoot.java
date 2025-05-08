9e02a0337c3342df2b82848f44a58a9e1d0d8e3d
harveyzhu27 <158126112+harveyzhu27@users.noreply.github.com>
2025-05-07 22:37:30 -0400

    First level changes

--- Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/GDXRoot.java
+++ Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/GDXRoot.java
@@ -180,7 +180,8 @@ public class GDXRoot extends Game implements ScreenListener {
                     Gdx.app.error("GDXRoot", "Invalid saved level: " + lastCompleted);
                 }
 
-                levels = new String[]{"move_intro","jump_intro","throw_intro","climb_intro","advanced_movement","box_intro","totem_intro","moth_intro","moth_medium","moth_medium_2"};
+                levels = new String[]{"move_intro","jump_intro","throw_intro","climb_intro","advanced_movement","box_intro","totem_intro","moth_intro","moth_medium","moth_medium_2","rune1","rune2","rainChallenge1", "rainChallenge2", "rainChallenge2"};
+//                levels = new String[]{"move_intro","move_intro","move_intro","move_intro","move_intro","box_intro","totem_intro","moth_intro","moth_medium","moth_medium_2","rune1","rune2","rainChallenge1", "rainChallenge2", "rainChallenge2"};
 
                 current = Math.min(nextLevel - 1, levels.length - 1);
 
@@ -238,7 +239,7 @@ public class GDXRoot extends Game implements ScreenListener {
 
             // Initialize the gameplay scene and level data.
             currentScene = new GameplayScene(directory, soundEngine, "platform");
-            levels = new String[]{"move_intro","jump_intro","throw_intro","climb_intro","advanced_movement","box_intro","totem_intro","moth_intro","moth_medium","moth_medium_2","rune1","rune2","rainChallenge1","rainChallenge2"};
+            levels = new String[]{"move_intro","jump_intro","throw_intro","climb_intro","advanced_movement","box_intro","totem_intro","moth_intro","moth_medium","moth_medium_2","rune1","rune2","rainChallenge1","rainChallenge2", "challenge15"};
             currentScene.loadLevel(levels[selectedLevel - 1], "rope_test");
             currentScene.setScreenListener(this);
             currentScene.setSpriteBatch(batch);
@@ -368,7 +369,6 @@ public class GDXRoot extends Game implements ScreenListener {
                 currentScene.setSpriteBatch(batch);
                 current = selectedLevel - 1;
                 setScreen(currentScene);
-
                 return;
             }
 
