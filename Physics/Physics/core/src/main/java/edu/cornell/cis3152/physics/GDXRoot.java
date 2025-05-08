96718149e3bf35c3e49f459b33696374af7bf084
EricJudys1 <egj29@cornell.edu>
2025-05-07 22:17:33 -0400

    light efit in tiled

--- Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/GDXRoot.java
+++ Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/GDXRoot.java
@@ -53,7 +53,7 @@ public class GDXRoot extends Game implements ScreenListener {
      */
     private GameplayScene[] controllers;
 
-    private String[] levels = new String[]{"move_intro","jump_intro","throw_intro","climb_intro","advanced_movement","box_intro","totem_intro","moth_intro","moth_medium","moth_medium_2"};
+    private String[] levels = new String[]{"move_intro","jump_intro","throw_intro","climb_intro","advanced_movement","box_intro","totem_intro","moth_intro","moth_medium","moth_medium_2", "rune1"};
 
     private GameplayScene currentScene;
     private LevelSelectScene levelSelectScene;
