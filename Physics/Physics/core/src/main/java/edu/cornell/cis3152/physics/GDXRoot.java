37b6d2214c55bc3ecd05f6c1f27dffc9b201a265
Hibiscus <jc2737@cornell.edu>
2025-05-16 18:53:33 -0400

    interMove level

--- Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/GDXRoot.java
+++ Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/GDXRoot.java
@@ -58,7 +58,7 @@ public class GDXRoot extends Game implements ScreenListener {
     private GameplayScene[] controllers;
 
 //    private String[] levels = new String[]{"move_intro","jump_intro","throw_intro","climb_intro","advanced_movement","box_intro","totem_intro","moth_intro","moth_medium","moth_medium_2", "rune_intro", "rune_medium", "rain_intro", "rain_medium", "rune_hard", "new_1", "new_2"};
-    private String[] levels = new String[]{"level1","level2","level3","level5","level4","level7","rune_medium","rune_advanced","rain_intro","new_2", "rune_hard", "rune_medium", "rain_intro", "rain_medium", ""};
+    private String[] levels = new String[]{"level1","level2","level3","interMove","level5","level4","level7","rune_medium","rune_advanced","rain_intro","new_2", "rune_hard", "rune_medium", "rain_intro", "rain_medium", "", };
     private GameplayScene currentScene;
     private String prevScreen;
     private LevelSelectScene levelSelectScene;
