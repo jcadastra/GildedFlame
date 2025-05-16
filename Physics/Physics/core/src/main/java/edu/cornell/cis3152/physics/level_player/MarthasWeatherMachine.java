9fb11df224c97d015df133ce18282fbce65e5cb6
EricJudys1 <egj29@cornell.edu>
2025-05-16 02:10:50 -0400

    fix fire in objects

--- Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/level_player/MarthasWeatherMachine.java
+++ Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/level_player/MarthasWeatherMachine.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.physics.box2d.RayCastCallback;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
 import edu.cornell.cis3152.physics.level_player.enemies.Enemy;
+import edu.cornell.cis3152.physics.level_player.enviromentals.Fire;
 import edu.cornell.cis3152.physics.level_player.player.Avatar;
 import edu.cornell.cis3152.physics.level_player.utils.CollisionFlag;
 import edu.cornell.cis3152.physics.level_player.utils.RainFlag;
@@ -158,7 +159,17 @@ public class MarthasWeatherMachine {
                     && !target.getObstacle().isSensor() && !target.getName().contains("grate") ) {
                     return false;
 
-                } else if ((target instanceof Enemy || target instanceof Avatar || target.getName().contains("platform") || target.getName().contains("burnable"))&& !target.getName().contains("grate")) {
+                } else if (
+                    (target instanceof Enemy
+                        || target instanceof Avatar
+                        || target.getName().contains("platform")
+                        || target.getName().contains("burnable"))
+                        && !target.getName().contains("grate")
+                        && !(
+                        sprite instanceof Fire
+                            && ((Fire)sprite).getBurntObstacle().hashCode() == target.hashCode()
+                    )
+                ) {
                     return false;
                 }
             }
