3ed34d60af3cbb33002dc9bce87e09edb7e85bd0
Judy <yz2429@cornell.edu>
2025-05-16 23:54:21 -0400

    fix

--- Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/LevelSelectScene.java
+++ Physics/Physics/core/src/main/java/edu/cornell/cis3152/physics/LevelSelectScene.java
@@ -473,7 +473,9 @@ public class LevelSelectScene implements Screen {
         stage.act(delta);
         updateDoorAnimation(delta);
         stage.draw();
-        if(inputController.xbox.getB()){listener.exitScreen(LevelSelectScene.this, 0);}
+        if (inputController.isUsingController()) {
+            if(inputController.xbox.getB()){listener.exitScreen(LevelSelectScene.this, 0);}
+        }
 
     }
 
